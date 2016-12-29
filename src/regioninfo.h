656d605eacda3fdc818231e246c9a463a03e178e
Sébastien De Fauw <sdefauw@gmail.com>
2016-12-29 11:52:14 +0100

    Support region information

---
+++ src/regioninfo.h
@@ -0,0 +1,24 @@
+/*
+ * Libphonenmuber for Asterisk
+ *
+ * Copyright (c) 2016.
+ *
+ * Sebastien De Fauw <sdefauw@gmail.com>
+ *
+ */
+
+#ifndef LIBASTPHONENUMBER_REGIONINFO_H
+#define LIBASTPHONENUMBER_REGIONINFO_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int get_country_code_for_region(char* region);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif //LIBASTPHONENUMBER_REGIONINFO_H

975eb6470cb4044b0989bd3af9d6214db6868b2a
Sebastien De Fauw <sfa@escaux.com>
2019-04-29 09:58:52 +0200

    Review testing section

--- test/display.c
+++ test/display.c
@@ -1,13 +1,4 @@
-
-#include <stdio.h>
-#include <stdlib.h>
-
-#include "../src/regioninfo.h"
-#include "../src/numinfo.h"
-#include "../src/formatting.h"
-
-#define BOOL2STR(b)     b ? "Yes" : "No"
-
+#include "display.h"
 
 void print_region_info(char* country) {
 
@@ -54,28 +45,4 @@ void print_formatted(char* num, char* country) {
 	printf(" - National compact: %s\n", buf);
 	num_format(num, country, NATIONAL_SHORT, buf);
 	printf(" - National short: %s\n", buf);
-}
-
-int main(int argc, char **argv)
-{
-	char* number;
-	char* country;
-
-	number = argv[1];
-	country = argv[2];
-
-	if (!number || !country) {
-		printf("Invalid input ! Input: <NUMBER> <COUNTRY>\n");
-		return 0;
-	}
-
-	printf("=== INPUT ===\n");
-	printf(" - Number: %s\n", number);
-	printf(" - Country: %s\n", country);
-
-	print_num_info(number, country);
-	print_region_info(country);
-	print_formatted(number, country);
-
-	return 0;
 }
---
+++ test/tester_static.c
@@ -0,0 +1,31 @@
+
+#include "display.h"
+
+#include "../src/regioninfo.h"
+#include "../src/numinfo.h"
+#include "../src/formatting.h"
+
+
+int main(int argc, char **argv)
+{
+	char* number;
+	char* country;
+
+	number = argv[1];
+	country = argv[2];
+
+	if (!number || !country) {
+		printf("Invalid input ! Input: <NUMBER> <COUNTRY>\n");
+		return 0;
+	}
+
+	printf("=== INPUT ===\n");
+	printf(" - Number: %s\n", number);
+	printf(" - Country: %s\n", country);
+
+	print_num_info(number, country);
+	print_region_info(country);
+	print_formatted(number, country);
+
+	return 0;
+}
